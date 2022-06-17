ec6c6c32c56cbbf3642605f50bed2918d75170e5
Anastasia <nastyajane@yandex.ru>
2022-06-17 20:02:27 +0300

    еще одна версия по коду

--- js/task_vue.js
+++ js/task_vue.js
@@ -12,33 +12,49 @@
     },
     methods: {
       searchMin(num) {
-         let num1 = num.split(" ").sort( (a, b) => a - b );
+         let num1 = num.split(" ");
          if (!num1.every((a) => /^(0|-?[1-9]\d*)$/.test(a))) {
             return this.error;
          } else {
-            let sum = parseInt(num1[0].trim()) + parseInt(num1[1].trim());
+            let min1 = parseInt(num1[0]);
+            let min2 = parseInt(num1[1]);
+            if (num1[0]>num1[1]) {
+               min2 = parseInt(num1[0]);
+               min1 = parseInt(num1[1]);
+            }
+            for (let q=2; q<num1.length; q++) {
+               if (parseInt(num1[q])<min2 && parseInt(num1[q])>= min1) {
+                  min2 = parseInt(num1[q]);
+               } 
+               if (parseInt(num1[q])<min1) {
+                  min2 = min1;
+                  min1 = parseInt(num1[q]);
+               } 
+            }
+            let sum = min1 + min2;
             return `Сумма двух минимальных чисел: ${sum}`;
          }
-         // вариант нахождения 2 минимальных значенией через Math.min:
-               // let num1 = num.split(" ");
-               // let min = Math.min(...num1);
-               // let sum = min + Math.min(...num1.splice(num1.indexOf(min), 1));
+         // вариант 2 нахождения минимальных значенией через sort
+            // let num1 = num.split(" ").sort( (a, b) => a - b );
+            // let sum = parseInt(num1[0]) + parseInt(num1[1]);
+
+         // вариант 3 нахождения  минимальных значенией через Math.min:
+            // let num1 = num.split(" ");
+            // let min = Math.min(...num1);
+            // let sum = min + Math.min(...num1.splice(num1.indexOf(min), 1));
       },
       sumOfMin() {
          this.text = "";
          if (this.num==false || this.num.includes(',')){
             this.text = this.error;
          } else {
-            this.testFunc();
-            this.text = this.searchMin(this.num);
+           this.text = this.searchMin(this.num);
          }
       },
-      testFunc(){
-         if (this.searchMin('1 0 2')!=='Сумма двух минимальных чисел: 1') {
-            throw new Error('Функция работает неверно')
-         }
-         if (this.searchMin('1 0  2')!=='Введите массив чисел через пробел') {
-            throw new Error('Функция работает неверно')
+      test_button() {
+         let button = document.getElementById('test');
+         if (button.addEventListener('click')) {
+            
          }
       }
    }
--- vue-test-utils-task/funcSum.js
+++ vue-test-utils-task/funcSum.js
@@ -27,13 +27,28 @@ export default {
       this.count++
     },
     searchMin(num) {
-      let num1 = num.split(" ").sort( (a, b) => a - b );
-      if (!num1.every((a) => /^(0|-?[1-9]\d*)$/.test(a))) {
-        return 'error';
-      } else {
-        let sum = parseInt(num1[0].trim()) + parseInt(num1[1].trim());
-        return sum;
-      }
+      let num1 = num.split(" ");
+        if (!num1.every((a) => /^(0|-?[1-9]\d*)$/.test(a))) {
+          return 'error';
+        } else {
+          let min1 = parseInt(num1[0]);
+          let min2 = parseInt(num1[1]);
+          if (num1[0]>num1[1]) {
+            min2 = parseInt(num1[0]);
+            min1 = parseInt(num1[1]);
+          }
+          for (let q=2; q<num1.length; q++) {
+            if (parseInt(num1[q])<min2 && parseInt(num1[q])>= min1) {
+              min2 = parseInt(num1[q]);
+            } 
+            if (parseInt(num1[q])<min1) {
+              min2 = min1;
+              min1 = parseInt(num1[q]);
+            } 
+          }
+          let sum = min1 + min2;
+          return sum;
+        }
     },
     sumOfMin() {
       this.text = "";
